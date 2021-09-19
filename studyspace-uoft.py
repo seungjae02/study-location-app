efbbd1850bbd5bb2ffc46a46e4592f38c143906b
eriysd <74118903+eriysd@users.noreply.github.com>
2021-09-18 23:10:09 -0400

    bubbles upgrade

--- studyspace-uoft.py
+++ studyspace-uoft.py
@@ -25,36 +25,21 @@ names = [item[0] for item in sheet.batch_get(('B2:B',))[0]]
 lats = [item[0] for item in sheet.batch_get(('C2:C',))[0]]
 longs = [item[0] for item in sheet.batch_get(('D2:D',))[0]]
 addresses = [item[0] for item in sheet.batch_get(('E2:E',))[0]]
-hover_txt = [name + '<br>' + address for name,
-             address in zip(names, addresses)]
+colors = [item[0] for item in sheet.batch_get(('G2:G',))[0]]
+hov_text = [item[0] for item in sheet.batch_get(('I2:I',))[0]]
+marker_sizes = [item[0] for item in sheet.batch_get(('J2:J',))[0]]
 
+for i in range(len(marker_sizes)):
+    marker_sizes[i] = int(marker_sizes[i])
 
 # app settings
-df = pd.read_csv("finalrecycling.csv")
 app = dash.Dash(__name__)
 blackbold = {'color': 'black', 'font-weight': 'bold'}
 
 app.layout = html.Div([
     # ---------------------------------------------------------------
-    # Map_legen + Borough_checklist + Recycling_type_checklist + Web_link + Map
     html.Div([
-        # html.Div([
-        # html.Ul([
-        #     html.Li("Compost", className='circle', style={'background': '#ff00ff', 'color': 'black',
-        #         'list-style': 'none', 'text-indent': '17px'}),
-        #     html.Li("Electronics", className='circle', style={'background': '#0000ff', 'color': 'black',
-        #                                                       'list-style': 'none', 'text-indent': '17px', 'white-space': 'nowrap'}),
-        #     html.Li("Hazardous_waste",  className='circle', style={'background': '#FF0000', 'color': 'black',
-        #                                                            'list-style': 'none', 'text-indent': '17px'}),
-        #     html.Li("Plastic_bags", className='circle', style={'background': '#00ff00', 'color': 'black',
-        #                                                        'list-style': 'none', 'text-indent': '17px'}),
-        #     html.Li("Recycling_bins", className='circle',  style={'background': '#824100', 'color': 'black',
-        #                                                           'list-style': 'none', 'text-indent': '17px'}),
-        # ], style={'border-bottom': 'solid 2px', 'border-color': 'RoyalBlue', 'padding-top': '6px'}
-        # ),
-
-        # Borough_checklist
-        # html.Label(children=['Borough: '], style=blackbold),
+        # Input Names
         dcc.Checklist(id='name',
                       options=[{'label': str(b), 'value': b}
                                for b in names],
@@ -62,8 +47,7 @@ app.layout = html.Div([
                       style={'display': 'none'}
                       ),
 
-        # Recycling_type_checklist
-        # html.Label(children=['Looking to recycle: '], style=blackbold),
+        # Input Addresses
         dcc.Checklist(id='address',
                       options=[{'label': str(b), 'value': b}
                                for b in addresses],
@@ -71,27 +55,20 @@ app.layout = html.Div([
                       style={'display': 'none'}
                       ),
 
-        # # Web_link
-        # html.Br(),
-        # html.Label(['Website:'], style=blackbold),
-        # html.Pre(id='web_link', children=[],
-        #          style={'white-space': 'pre-wrap', 'word-break': 'break-all',
-        #                 'border': '1px solid black', 'text-align': 'center',
-        #                 'padding': '12px 12px 12px 12px', 'color': 'blue',
-        #                 'margin-top': '3px'}
-        #          ),
 
     ],  className='three columns'
     ),
 
+    html.Button('Submit', id='submit-val', n_clicks=0),
+
     # Map
     html.Div([
+
         dcc.Graph(id='graph', config={'displayModeBar': False, 'scrollZoom': True},
                   style={'background': 'pink', 'padding-bottom': '2px',
                          'height': '100vh'}
                   )
-        # ], className='nine columns'
-        # ),
+
 
     ], className='row'
     ),
@@ -113,11 +90,11 @@ def update_figure(name, address):
         lon=longs,
         lat=lats,
         mode='markers',
-        marker={'color': "Navy"},
-        unselected={'marker': {'opacity': 1}},
-        selected={'marker': {'opacity': 0.5, 'size': 25}},
+        marker={'opacity': 0.5, 'color': colors, 'size': marker_sizes},
+        unselected={'marker': {'opacity': 0.5}},
+        selected={'marker': {'opacity': 1.0, 'size': 50}},
         hoverinfo='text',
-        hovertext=hover_txt,
+        hovertext=hov_text
     )]
 
     # Return figure
